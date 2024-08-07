099fcc6cdbd8af112dd7370dbadba4fba3de330b
Yan da Costa <yan.kostasc@gmail.com>
2024-08-07 17:09:01 -0300

    Initial commit

---
+++ backend/loan-calculator/src/test/java/com/loancalculator/loan_calculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loancalculator.loan_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
